fed46c789cbf2eaf55d160430c122d9f621194ce
htao <htao@thoughtworks.com>
2018-08-25 17:37:47 +0800

    [taohui] add style sheet

--- src/components/home/PresentationalComponent.js
+++ src/components/home/PresentationalComponent.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const PresentationalComponent = (props) => {
 	return (
 		<View>
-			<Text onPress = {props.updateState}>
+			<Text onPress = {props.updateState} style = {styles.myState}>
 				{props.myState}
 			</Text>
 		</View>
 		)
 }
+
+const styles = StyleSheet.create ({
+	myState: {
+		marginTop: 20,
+		textAlign: 'center',
+		color: 'blue',
+		fontWeight: 'bold',
+		fontSize: 20
+	}
+})
 
 export default PresentationalComponent;
