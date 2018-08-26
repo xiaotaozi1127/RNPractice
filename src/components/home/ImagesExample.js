de13fb1ac562b8d69b0c69ea21e1f2ca7d906af1
htao <htao@thoughtworks.com>
2018-08-26 11:05:07 +0800

    [taohui] display network image

--- src/components/home/ImagesExample.js
+++ src/components/home/ImagesExample.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import { Image } from 'react-native'
 
 const Home = () => (
-   <Image source = {require('../../img/bridge.jpg')} 
+   <Image source = {{ uri: 
+         'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png' }} 
    		  style = {{ width: 200, height: 200 }}/>
 )
 export default Home
