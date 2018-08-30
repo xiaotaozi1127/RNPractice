ff4b4e5db555026e4abe98e1428d8dfd8d13802a
htao <htao@thoughtworks.com>
2018-08-30 16:24:14 +0800

    [taohui] refactor: extract render item as a function

--- src/components/home/HorizonFlatList.js
+++ src/components/home/HorizonFlatList.js
@@ -2,10 +2,26 @@ import React, {Component} from 'react';
 import {FlatList, View, Text, Dimensions} from 'react-native';
 import { compact, isEmpty, isNull, map, times } from "lodash";
 
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
 export default class HorizonFlatList extends Component {
+	_renderItem = ({item}) => (
+	    				<View style = {{backgroundColor: item.color, 
+								flex: 1,
+								marginTop: 20,
+								width: screenWidth,
+								justifyContent: 'center',
+								alignItems: 'center'
+								}}>
+
+							<Text style={{color: 'white', fontSize: 20, padding: 15}}>
+								{item.text}
+							</Text>
+						</View>
+  	);
+
 	render() {
-		let screenWidth = Dimensions.get('window').width;
-		let screenHeight = Dimensions.get('window').height;
 		return (
 			<FlatList
 				data={[
@@ -24,18 +40,7 @@ export default class HorizonFlatList extends Component {
 						color: '#663399',
 						text: 'screen3'
 					}]}
-  				renderItem={({item}) => <View style = {{backgroundColor: item.color, 
-								flex: 1,
-								marginTop: 20,
-								width: screenWidth,
-								justifyContent: 'center',
-								alignItems: 'center'
-								}}>
-
-							<Text style={{color: 'white', fontSize: 20, padding: 15}}>
-								{item.text}
-							</Text>
-						</View>}
+  				renderItem={this._renderItem}
   			/>
 			)
 	}
