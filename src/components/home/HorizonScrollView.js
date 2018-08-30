775b3dde403d8152eb450daf7621e5655752b054
htao <htao@thoughtworks.com>
2018-08-30 16:10:44 +0800

    [taohui] use array to gennerate scroll view elements

--- src/components/home/HorizonScrollView.js
+++ src/components/home/HorizonScrollView.js
@@ -1,46 +1,40 @@
 import React, {Component} from 'react';
 import {ScrollView, View, Text, Dimensions} from 'react-native';
+import { compact, isEmpty, isNull, map, times } from "lodash";
 
 export default class HorizonScrollView extends Component {
 	render () {
 		let screenWidth = Dimensions.get('window').width;
 		let screenHeight = Dimensions.get('window').height;
+		let children = [{
+			color: '#5f9ea0',
+			text: 'screen1'
+		}, {
+			color: 'tomato',
+			text: 'screen2'
+		},
+		{
+			color: '#663399',
+			text: 'screen3'
+		}]
 		return (
 			<ScrollView horizontal={true} pagingEnabled={true}>
-				<View style = {{backgroundColor: '#5f9ea0', 
+				{
+					map(children, (child, index) => (
+						<View style = {{backgroundColor: child.color, 
 								flex: 1,
 								marginTop: 20,
 								width: screenWidth,
 								justifyContent: 'center',
 								alignItems: 'center'
 								}}>
-					<Text style={{color: 'white', fontSize: 20, padding: 15}}>
-						Screen 1
-					</Text>
-				</View>
-				<View style = {{backgroundColor: 'tomato', 
-								flex: 1,
-								marginTop: 20,
-								width: screenWidth,
-								justifyContent: 'center',
-								alignItems: 'center'
-								}}>
-					<Text style={{color: 'white', fontSize: 20, padding: 15}}>
-						Screen 2
-					</Text>
-				</View>
-				<View style = {{backgroundColor: '#663399', 
-								flex: 1,
-								marginTop: 20,
-								width: screenWidth,
-								justifyContent: 'center',
-								alignItems: 'center'
-								}}>
-					<Text style={{color: 'white', fontSize: 20, padding: 15}}>
-						Screen 3
-					</Text>
-				</View>
 
+							<Text style={{color: 'white', fontSize: 20, padding: 15}}>
+								{child.text}
+							</Text>
+						</View>
+					))
+				}
 			</ScrollView>
 		);
 	}
